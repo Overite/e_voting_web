ffd416bc4ece420fbf8288e8129a4b652335c049
Victor <vicky.robilson5528@gmail.com>
2024-04-02 12:03:14 +0100

    active set for the sidebar

--- client/src/javascript/main.js
+++ client/src/javascript/main.js
@@ -1,30 +1,55 @@
-let openSidebar = document.querySelector('.opensidebar');
-let closeSidebar = document.querySelector('.closesidebar');
+let openSidebar = document.querySelector('.opensidebar')
+let closeSidebar = document.querySelector('.closesidebar')
 let sidebar = document.querySelector('.sidebar');
 let header = document.querySelector('.header');
 
-function setActive(index){
-  const menuItems = document.getElementsByClassName('menu-item');
-
-  for (var i = 0; i < menuItems.length; i++){
-    if(i === index){
-      menuItems[i].classList.add('active')
-    } else {
-      menuItems[i].classList.remove('active')
-    }
-  }
-}
 
 
-openSidebar.addEventListener('click',function (){
+openSidebar?.addEventListener('click',function(){
   sidebar.style.left ='0';
   header.style.left = '313px'
 })
 
-closeSidebar.addEventListener('click',()=>{
+closeSidebar?.addEventListener('click',function(){
   sidebar.style.left ='-313px';
   header.style.left = '0'
 })
+
+const faceId = (elId,msg)=>{
+  const fname = 'victor'
+  let elementId = document.getElementById(elId);
+  let btn = elementId;
+  btn?.addEventListener('click', function(){
+  if(fname === 'victor'){
+    // console.log(msg)
+    elementId.textContent = msg;
+  }else {
+    elementId.textContent = msg;
+  }
+  })
+}
+faceId("facialIcon","hello victor")
+
+document.addEventListener('DOMContentLoaded', function(){
+  const currentLocation = window.location.href;
+  const sidebarIcons = document.querySelectorAll('img')
+  const sidebarlinks = document.querySelectorAll('li a');
+
+  sidebarlinks.forEach((link)=>{
+    if(link.href === currentLocation){
+      link.classList.add('active')
+    }
+  })
+
+  sidebarIcons.forEach((img)=>{
+    if(img.src){
+      img.classList.add('image')
+    }
+  })
+})
+
+
+
 
 const users = [
   { id:1,
@@ -114,7 +139,7 @@ const displayUsers = (filteredUsers) => {
 }
 
 
-document.getElementById('searchInput').addEventListener('input',(e)=>{
+document.getElementById('searchInput')?.addEventListener('input',function(e){
   const query = e.target.value.trim();
   const userList = document.getElementById('userList')
   // const userDetails = document.getElementById('userDetails')
@@ -145,51 +170,117 @@ document.getElementById('searchInput').addEventListener('input',(e)=>{
 // toggleInput.src = type === 'password' ? './src/assets/icons/eye-slash.svg' :'./src/assets/icons/eye-regular.svg';
 // })
 
-// document.addEventListener('DOMContentLoaded', () => {
-// const loginForm = document.getElementById('loginForm');
-// loginForm.addEventListener('submit', (e) => {
-//   console.log('hello word')
-//     e.preventDefault();
-//     function errorInput(elementId,message){
-//       const errorElementId = document.getElementById(elementId);
-//       errorElementId.textContent = message;
-//     }
-//     function clearError(elementId){
-//       const clearElementId = document.getElementById(elementId);
-//       clearElementId.textContent = '';
-//     }
+document.addEventListener('DOMContentLoaded', () => {
+const loginForm = document.getElementById('loginForm');
+loginForm?.addEventListener('submit', (e) => {
+  // console.log('hello word')
+
+    e.preventDefault();
+    function errorInput(elementId,message){
+      const errorElementId = document.getElementById(elementId);
+      errorElementId.textContent = message;
+      errorElementId.style.display = 'block';  
+      setTimeout(() => {
+        errorElementId.style.display = 'none'
+      }, 2000);
+    }
+
+    function ThankYou(eId,text){
+      const markId = document.getElementById(eId);
+      markId.textContent = text;
+      markId.style.display = 'block';  
+      setTimeout(() => {
+        markId.style.display = 'none'
+      }, 2000);
+    }
+
+    function clearError(elementId){
+      const clearElementId = document.getElementById(elementId);
+      clearElementId.textContent = '';
+    }
+
     
-//     clearError('admin')
-//     clearError('password')
-
-//     const admin = document.getElementById('admin');
-//     const password = document.getElementById('password');
-//     const adminInput = admin.value;
-//     const passwordInput = password.value;
-
-//     const toggleInput = document.getElementById('togglePassword');
-//     toggleInput.addEventListener('click', ()=>{
-//     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-//     password.setAttribute('type', type);
-//     toggleInput.src = type === 'password' ? './src/assets/icons/eye-slash.svg' :'./src/assets/icons/eye-regular.svg';
-//     })
-
-// if(!adminInput){
-//   errorInput('errorInput', 'Admin is required')
-//   return;
-// }
     
+    clearError('admin')
+    clearError('password')
 
-// if(!passwordInput){
-//   errorInput('errorInput', 'password is required')
-//   return;
+    const admin = document.getElementById('admin');
+    const password = document.getElementById('password');
+    const adminInput = admin.value;
+    const passwordInput = password.value;
+
+    const toggleInput = document.getElementById('togglePassword');
+    toggleInput.addEventListener('click', ()=>{
+    // const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    // password.setAttribute('type', type);
+    // toggleInput.src = type === 'password' ? './src/assets/icons/eye-slash.svg' :'./src/assets/icons/eye-regular.svg';
+    console.log("victor")
+    })
+
+if(!adminInput){
+  errorInput('errorInput', 'Admin is required')
+  return;
+}
+    
+if(!passwordInput){
+  errorInput('errorInput', 'password is required')
+  return;
+}
+
+if(adminInput && passwordInput){
+  ThankYou('markInput', 'you are logging in')
+  window.location=`dashboard.html`;
+  return
+}
+  })
+})
+
+// const headerItems = [
+//   {
+//     headTitle:"Dashboard",
+//   },
+//   {
+//     headTitle:"Register voting",
+//   },
+//   {
+//     headTitle:"Voters",
+//   }
+// ]
+
+// const displayHeader = (header) => {
+//   const headerContainer = document.querySelector('header')
+//   headerContainer.innerHTML = `  
+//   <header class="header">
+//   <div class="header-row">
+//       <img src="./src/assets/icons/menu.svg" class="opensidebar" alt="menu">
+//       <h3 class="header-row--title">${header?.headTitle}</h3>
+//   </div>
+//   <div class="header-row--metamask">
+//       <select name="connect" id="">
+//           <option value="connect">Connected</option>
+//       </select>
+//       <div class="">
+//           <img src="./src/assets/icons/chevron-down.svg" alt="">
+//       </div>
+//   </div>
+//   </header>
+//   `;
 // }
 
-//   })
-// })
+// displayHeader(headerItems)
 
-
-
+const displayUserDetail = (user) => {
+  const userDetails = document.getElementById('userDetails')
+  userDetails.innerHTML = `<img src="${user.image}" height="40" width="40" alt="${user.fullName}" class="user-img">`;
+  userDetails.innerHTML += `<div style="display:flex;flex-direction:column"><h2 style="font-size:16px;font-weight:700;line-height:18.38px;">Full Name:</h2><h3 style="font-size:16px;font-weight:400;line-height:18.38px;color:#0C0908;">${user.fullName}</h3></div>`;
+  userDetails.innerHTML += `<div style="display:flex;flex-direction:column"><h2 style="font-size:16px;font-weight:700;line-height:18.38px;">Voter's Identification Number:</h2><p style="font-size:16px;font-weight:400;line-height:18.38px;color:#0C0908;">${user.VotersId}</p></div>`;
+  userDetails.innerHTML += `<div style="display:flex;flex-direction:column"><h2 style="font-size:16px;font-weight:700;line-height:18.38px;">Date Of Birth:</h2><p style="font-size:16px;font-weight:400;line-height:18.38px;color:#0C0908;">${user.dateOfBirth}</p></div>`;
+  userDetails.innerHTML += `<div style="display:flex;flex-direction:column"><h2 style="font-size:16px;font-weight:700;line-height:18.38px;">Date Of Registration:</h2><p style="font-size:16px;font-weight:400;line-height:18.38px;color:#0C0908;">${user.dateOfReg}</p></div>`;
+  userDetails.innerHTML += `<div style="display:flex;flex-direction:column"><h2 style="font-size:16px;font-weight:700;line-height:18.38px;">Occupation:</h2><p style="font-size:16px;font-weight:400;line-height:18.38px;color:#0C0908;">${user.Occupation}</p></div>`;
+  userDetails.innerHTML += `<div style="display:flex;flex-direction:column"><h2 style="font-size:16px;font-weight:700;line-height:18.38px;">Gender:</h2><p style="font-size:16px;font-weight:400;line-height:18.38px;color:#0C0908;">${user.Gender}</p></div>`;
+  userDetails.innerHTML += `<div style="display:flex;flex-direction:column"><h2 style="font-size:16px;font-weight:700;line-height:18.38px;">Address:</h2><p style="font-size:16px;font-weight:400;line-height:18.38px;color:#0C0908;">${user.address}</p></div>`;
+  userDetails.innerHTML += `<img src="${user.qrcode}" alt="${user.fullName}" class="user-img">`;
+}
 
 
 
